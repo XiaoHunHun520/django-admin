0273f053c2a201d828ff9cdf4592ef869f86a245
Penguin <129220411+XiaoHunHun520@users.noreply.github.com>
2025-03-26 04:04:20 +0800

    开始配置已这个为准

--- djangoCms/djangoCms/asgi.py
+++ djangoCms/djangoCms/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for djangoCms project.
+ASGI config for djangocms project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangoCms.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangocms.settings')
 
 application = get_asgi_application()
--- djangoCms/djangoCms/urls.py
+++ djangoCms/djangoCms/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for djangoCms project.
+URL configuration for djangocms project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
